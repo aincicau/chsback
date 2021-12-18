500b68354e16fdf28e7e6555f1ace3ee3ac73b28
Alexandru Incicau <aincicau@yahoo.com>
2021-12-18 17:46:28 +0200

    Added router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module chsback
 
 go 1.16
+
+require github.com/go-chi/chi v1.5.4
--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package chsback
+
+import (
+	"github.com/go-chi/chi"
+)
+
+func main() {
+	router := chi.NewRouter()
+
+	router.Route("/", func(r chi.Router) {
+		r.Get("/history", ...)
+	})
+}
