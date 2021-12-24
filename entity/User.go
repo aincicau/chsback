4fbf24a088047e1a072af0132778873ac8978caf
Alexandru Incicau <aincicau@yahoo.com>
2021-12-24 17:38:11 +0200

    Modified User entity

--- entity/User.go
+++ entity/User.go
@@ -2,6 +2,7 @@ package entity
 
 type User struct {
 	ID       string `gorm:"type:uuid;default:uuid_generate_v4();primary_key;column:id"`
+	Username string `gorm:"type:varchar(255);not null;unique"`
 	Email    string `gorm:"type:varchar(255);not null;unique"`
 	Password string `gorm:"type:varchar(255);not null"`
 }
