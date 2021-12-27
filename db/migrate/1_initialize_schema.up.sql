59f3410def9d16d7c83f238a91e019fe90c754be
Alexandru Incicau <aincicau@yahoo.com>
2021-12-27 17:58:57 +0200

    Created table for history entries in database

--- db/migrate/1_initialize_schema.up.sql
+++ db/migrate/1_initialize_schema.up.sql
@@ -9,4 +9,14 @@ CREATE TABLE users (
     CONSTRAINT pk_user_id PRIMARY KEY (id)
 );
 
+CREATE TABLE histories (
+    id uuid DEFAULT uuid_generate_v4(),
+    file_name varchar(255) NOT NULL,
+    date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    result varchar(255) NOT NULL,
+    user_id uuid NOT NULL,
+    CONSTRAINT pk_history_id PRIMARY KEY (id),
+    CONSTRAINT fk_history_user FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
 COMMIT;
--- entity/History.go
+++ entity/History.go
@@ -4,7 +4,7 @@ import "time"
 
 type History struct {
 	ID       string    `gorm:"type:uuid;default:uuid_generate_v4();primary_key;column:id"`
-	FileName string    `gorm:"type:varchar(255);not null"`
+	FileName string    `gorm:"type:varchar(255);not null;column:file_name" json:"file_name"`
 	Date     time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;not null"`
 	Result   string    `gorm:"type:varchar(255);not null"`
 	UserID   string    `gorm:"type:uuid;not null;column:user_id" json:"user_id"`
