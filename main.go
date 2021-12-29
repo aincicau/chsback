18700ef304826bd34670ca990308fcf5a3a0d005
Alexandru Incicau <aincicau@yahoo.com>
2021-12-29 22:12:42 +0200

    Added model evaluation(needs replacing with our values)

--- main.go
+++ main.go
@@ -4,15 +4,28 @@ import (
 	"chsback/config"
 	"chsback/db"
 	"chsback/rest"
+	"fmt"
 	"log"
 	"net/http"
+
+	tf "github.com/galeone/tensorflow/tensorflow/go"
+	tg "github.com/galeone/tfgo"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 )
 
 func main() {
-	// model := tg.LoadModel("model/output/keras", []string{"serve"}, nil)
+	model := tg.LoadModel("model/output/keras", []string{"serve"}, nil)
+	fakeInput, _ := tf.NewTensor([1][28][28][1]float32{})
+	results := model.Exec([]tf.Output{
+		model.Op("StatefulPartitionedCall", 0),
+	}, map[tf.Output]*tf.Tensor{
+		model.Op("serving_default_inputs_input", 0): fakeInput,
+	})
+	predictions := results[0]
+	fmt.Println(predictions.Value())
+
 	err := config.InitConfig()
 	if err != nil {
 		log.Fatal("Not able to create config")
