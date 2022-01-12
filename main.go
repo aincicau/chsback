0c84f9446da46e8de653a13471c5d75f0709c647
Alexandru Incicau <aincicau@yahoo.com>
2022-01-12 16:02:42 +0200

    History creation endpoint done

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 		r.Post("/user", rest.CreateUser)
 		r.Post("/login", rest.Login)
 		r.Get("/history", rest.GetHistory)
+		r.Post("/history", rest.PostHistory)
 		r.Get("/logout", rest.Logout)
 	})
 
--- rest/History.go
+++ rest/History.go
@@ -6,6 +6,7 @@ import (
 	"chsback/entity"
 	"chsback/utils"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -48,5 +49,49 @@ func GetHistory(rw http.ResponseWriter, r *http.Request) {
 		}
 
 		rw.Write(dataToBeSent)
+	}
+}
+
+func PostHistory(rw http.ResponseWriter, r *http.Request) {
+	sessionDetails, err := store.Get(r, "cookieMonster")
+
+	if utils.HasError(err, common.INTERNAL_SERVER_ERROR) {
+		http.Error(rw, "Cookie error", http.StatusInternalServerError)
+		return
+	}
+
+	if sessionDetails.IsNew {
+		http.Error(rw, common.AUTHENTICATION_NEEDED+common.PLEASE_LOGIN, http.StatusUnauthorized)
+		return
+	} else {
+		sessionID := (sessionDetails.Values["sessionID"]).(string)
+
+		var sessionDB entity.Session
+		result := db.GetDB().Find(&sessionDB, "id=?", sessionID)
+		if result.RecordNotFound() || result.RowsAffected == 0 {
+			http.Error(rw, common.AUTHENTICATION_NEEDED+common.PLEASE_LOGIN, http.StatusUnauthorized)
+			return
+		}
+
+		reqBody := r.Body
+		bodyBytes, err := ioutil.ReadAll(reqBody)
+		if utils.HasError(err, common.READ_DATA_ERROR) {
+			http.Error(rw, common.INTERNAL_SERVER_ERROR, http.StatusInternalServerError)
+			return
+		}
+
+		var history entity.History
+		err = json.Unmarshal(bodyBytes, &history)
+		if utils.HasError(err, common.UNMARSHAL_ERROR) {
+			http.Error(rw, common.DATA_INTEGRITY_ERROR, http.StatusBadRequest)
+			return
+		}
+		history.UserID = sessionDB.UserID
+
+		result = db.GetDB().Create(&history)
+		if result.Error != nil {
+			http.Error(rw, "Unable to add history", http.StatusInternalServerError)
+			return
+		}
 	}
 }
